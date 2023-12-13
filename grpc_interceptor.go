ca9ec44a0627e1ba4163be32b305f535c7f3f07d
lidonglin@rcrai.com <lidonglin@rcrai.com>
2023-12-13 17:59:13 +0800

    update grpc interceptor

--- grpc_interceptor.go
+++ grpc_interceptor.go
@@ -10,13 +10,17 @@ package tgserver
 
 import (
 	"context"
+	"runtime"
 	"strings"
 	"time"
 
+	"github.com/choveylee/tlog"
 	"github.com/choveylee/tmetric"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
 )
 
 const (
@@ -26,8 +30,40 @@ const (
 	BidiStream   = "bidi_stream"
 )
 
+func funcFileLine(excludePKG string) (string, string, int) {
+	const depth = 8
+	var pcs [depth]uintptr
+	n := runtime.Callers(3, pcs[:])
+	ff := runtime.CallersFrames(pcs[:n])
+
+	var fn, file string
+	var line int
+	for {
+		f, ok := ff.Next()
+		if !ok {
+			break
+		}
+
+		fn, file, line = f.Function, f.File, f.Line
+		if !strings.Contains(fn, excludePKG) {
+			break
+		}
+	}
+
+	if index := strings.LastIndexByte(fn, '/'); index != -1 {
+		fn = fn[index+1:]
+	}
+
+	return fn, file, line
+}
+
 func recoveryHandler(ctx context.Context, r interface{}) error {
-	return status.Errorf(codes.Unknown, "recover from %v", r)
+	_, file, line := funcFileLine("github.com/choveylee")
+
+	errMsg := tlog.E(ctx).Msgf("recover from panic (%s, %d, %v).",
+		file, line, r)
+
+	return status.Error(codes.Unknown, errMsg)
 }
 
 func splitMethodName(fullMethod string) (string, string) {
@@ -50,7 +86,50 @@ func logServerInterceptor(ctx context.Context, req interface{}, info *grpc.Unary
 
 	service, method := splitMethodName(info.FullMethod)
 
-	logFormatter(ctx, service, method, req, resp, err)
+	code := status.Code(err)
+
+	latency := time.Since(GetStartTime(ctx))
+
+	var event *tlog.Tevent
+
+	if code == codes.OK {
+		event = tlog.D(ctx)
+	} else {
+		event = tlog.E(ctx).Err(err)
+	}
+
+	event = event.Detailf("service:%s", service).
+		Detailf("method:%s", method).
+		Detailf("latency:%v", latency).
+		Detailf("code:%s", code.String())
+
+	reqData := make([]byte, 0)
+	respData := make([]byte, 0)
+
+	request, ok := req.(proto.Message)
+	if ok {
+		var err error
+
+		reqData, err = protojson.Marshal(request)
+		if err != nil {
+			tlog.W(ctx).Err(err).Msgf("marshal req proto message err (%v).", err)
+		}
+	}
+
+	response, ok := resp.(proto.Message)
+	if ok {
+		var err error
+
+		respData, err = protojson.Marshal(response)
+		if err != nil {
+			tlog.W(ctx).Err(err).Msgf("marshal resp proto message err (%v).", err)
+		}
+	}
+
+	event = event.Detailf("req:%s", string(reqData)).
+		Detailf("resp:%s", string(respData))
+
+	event.Msg("grpc access log")
 
 	return resp, err
 }
